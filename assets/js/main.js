39bc9473d2eb6a699c159a62aff32ec707f2e1c9
JuanPujol81 <raul810324@gmail.com>
2022-02-27 19:30:38 +0000

    Addin a burger menu for movile devices

---
+++ assets/js/main.js
@@ -0,0 +1,9 @@
+    const ham = document.querySelector('.ham')
+    const enlaces = document.querySelector('.menu')
+    ham.addEventListener('click', () => {
+        enlaces.classList.toggle('activado')
+    })
+
+    ham.addEventListener('touchmove', () => {
+        enlaces.classList.toggle('desactivado')
+    })
